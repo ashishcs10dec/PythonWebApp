f3a776e8bfc4d6ed9652bbcf62810128a49996e8
ashishcs10dec <ashishsharansrivastava@gmail.com>
2023-07-28 15:25:36 +0530

    second commit

--- app.py
+++ app.py
@@ -41,5 +41,13 @@ def testimonial():
 def contact():
     return render_template('contact.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__=='__main__':
     app.run()
